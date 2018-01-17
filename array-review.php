c5ca8a4eaabbffe10c51695ff5bf6ffdb067d7e3
Dracologist <ejckanzler@yahoo.com>
2018-01-16 20:29:22 -0500

    initialized flavors associative array

--- array-review.php
+++ array-review.php
@@ -8,6 +8,9 @@
 
 $animals = array("panda", "alpaca", "boa");
 
+$flavors = array("grasshopper" => "The Grasshopper", "maple" => "Whiskey Maple Bacon", "carrot" => "Carrot Walnut",
+    "caramel" => "Salted Caramel Cupcake", "velvet" => "Red Velvet", "lemon" => "Lemon Drop", "tiramisu" => "Tiramisu");
+
 function sortPrint() {
     global $animals;
     sort($animals);
